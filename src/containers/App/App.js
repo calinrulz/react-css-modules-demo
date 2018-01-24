6c25072e1535e5781b7ec1fcfdc1626054e0194d
Calin Georgescu <calinrulz@gmail.com>
2018-01-24 20:24:11 +0200

    first commit

---
+++ src/containers/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import styles from './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h2>
+            Congrats, you now have Sass and CSS Modules in your React project!
+            <span role="img" aria-label="emoji"> 🤑 </span>
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
